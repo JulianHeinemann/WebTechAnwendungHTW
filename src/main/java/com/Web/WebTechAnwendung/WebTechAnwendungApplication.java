4400289222cf6e93bcf09d48b60388fc98dd3ba5
julianheinrich <julianhallo22@gmail.com>
2021-10-23 07:58:36 +0200

    Initial Commit, all Usefull Data.

---
+++ src/main/java/com/Web/WebTechAnwendung/WebTechAnwendungApplication.java
@@ -0,0 +1,13 @@
+package com.Web.WebTechAnwendung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTechAnwendungApplication {
+
+	public static void main(String[] args) { SpringApplication.run(WebTechAnwendungApplication.class, args);}
+
+
+
+}
