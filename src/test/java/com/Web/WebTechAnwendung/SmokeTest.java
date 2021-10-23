844c9ee1b37c70d1ac4ff9779b8e10d1bd53ca0f
julianheinrich <julianhallo22@gmail.com>
2021-10-23 07:55:03 +0200

    Testing: ControllerTest, HTTPRequestTest, WebApplication Test

---
+++ src/test/java/com/Web/WebTechAnwendung/SmokeTest.java
@@ -0,0 +1,20 @@
+package com.Web.WebTechAnwendung;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    private GreetingControler controller;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+}
