1c3596e6f75d953ee12324854ef54fd438b6ea23
julianheinrich <julianhallo22@gmail.com>
2021-11-12 14:01:24 +0100

    Test für GitHub Actions und H2 Datenbank

---
+++ src/test/java/com/Web/WebTechAnwendung/CocktailTest.java
@@ -0,0 +1,14 @@
+package com.Web.WebTechAnwendung;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CocktailTest {
+
+    @Test
+    void contextLoads(){
+
+    }
+}
